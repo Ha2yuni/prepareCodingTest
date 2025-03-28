4405ba04023f3ba8c906751146ff8e7ac46dc120
Ha2yunii <godbs4710@naver.com>
2025-03-28 22:11:57 +0900

    Title: 문자 출력하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250328/문자 출력하기/print-text.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    char cEnter(' ');
+
+    cin >> cEnter;
+
+    for (int i=0; i<8; i++) {
+        cout << cEnter;
+    }
+    return 0;
+}
