c96f54ffe7d389756deae382edbf78b2d671fba7
Ha2yunii <godbs4710@naver.com>
2025-03-28 22:25:21 +0900

    Title: 문자열 출력하기, Time: 7ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 250328/문자열 출력하기/print-string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int N(0);
+
+    for (int i=0; i<N; i++) 
+        cout << "LeebrsCode";
+    return 0;
+}
