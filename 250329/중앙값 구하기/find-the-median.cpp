e9d9bd195d73d1200d40552fa378b69718944fb7
Ha2yunii <godbs4710@naver.com>
2025-03-29 18:04:08 +0900

    Title: 중앙값 구하기, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250329/중앙값 구하기/find-the-median.cpp
+++ 250329/중앙값 구하기/find-the-median.cpp
@@ -4,18 +4,33 @@ using namespace std;
 int main() {
     // Please write your code here.
     int A(0), B(0), C(0);
-    int bigger(0);
+    int biggest(0), smallest(0);
 
     cin >> A >> B >> C;
+    
+    biggest = A;
+    smallest = A;
 
-    if ((A > B) && (A < C))
+    if (biggest < B) {
+        biggest = B;
+        if (biggest < C)
+            biggest = C;
+    } else if (biggest < C)
+            biggest = C;
+        
+    if (smallest > B) {
+        smallest = B;
+        if (smallest > C)
+            smallest = C;
+    } else if (smallest > C)
+            smallest = C;
+
+    if (A != biggest && A != smallest)
         cout << A;
-    else if ((A < B) && (A > C))
-        cout << A;
-    else if (((A < B) && (A < C)) && (B < C))
+    else if (B != biggest && B != smallest)
         cout << B;
     else
         cout << C;
-        
+
     return 0;
 }
