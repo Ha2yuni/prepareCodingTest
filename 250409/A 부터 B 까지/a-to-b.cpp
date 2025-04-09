beb111cc8c35fd4e6c123fc43f55ef147fd15175
Ha2yuni <godbs4710@naver.com>
2025-04-09 17:11:04 +0900

    Title: A 부터 B 까지, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/A 부터 B 까지/a-to-b.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int a(0), b(0);
+
+    cin >> a >> b;
+
+    while (a <= b) {
+
+        cout << a << " ";
+
+        if (a%2==0)
+            a += 3;
+        else
+            a *= 2;
+
+
+    }
+
+    return 0;
+}
