a7f25df8df002d5d36a781b3062e601d10656f02
Ha2yuni <godbs4710@naver.com>
2025-04-09 17:06:32 +0900

    Title: 0 나올때까지 출력, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250409/0 나올때까지 출력/print-until-zero-is-given.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num(-1);
+
+    while (num != 0) {
+        cin >> num;
+        if (num != 0)
+            cout << num << endl;
+    }
+
+    return 0;
+}
