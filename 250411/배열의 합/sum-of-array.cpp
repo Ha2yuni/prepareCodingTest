3e9e2b499a623258d2c5939331618f28f5ea5298
Ha2yuni <godbs4710@naver.com>
2025-04-11 13:04:53 +0900

    Title: 배열의 합, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 250411/배열의 합/sum-of-array.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+
+    int num[4][4];
+    int total(0);
+
+    for (int i=0; i<4; i++) {
+        for (int j=0; j<4; j++) {
+            cin >> num[i][j];
+            total += num[i][j];
+        }
+        cout << total << endl;
+        total = 0;
+    }
+
+    return 0;
+}
